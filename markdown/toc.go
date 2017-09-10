bf3ba2b1a63720ab6284ae9a2f4d49be0d15a9fe
bigzhu <vermiliondun@gmail.com>
2017-09-11 00:26:46 +0800

    加入id-前缀

--- markdown/toc.go
+++ markdown/toc.go
@@ -18,7 +18,7 @@ func FormatToc(title string) string { // 生成 toc 的 markdown 格式
 	title = strings.TrimSpace(title)
 	// fmt.Printf("t%st\n", title)
 	escaped_title := url.QueryEscape(title)
-	return fmt.Sprintf("%s* [%s](#%s)\n", space, title, escaped_title)
+	return fmt.Sprintf("%s* [%s](#id-%s)\n", space, title, escaped_title)
 }
 
 func CreateToc(content string) string { //取出 toc
