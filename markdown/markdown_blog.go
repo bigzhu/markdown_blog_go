a1f7a5f9e87d25a08ba208a1b9357c4cbb0a1b7c
bigzhu <vermiliondun@gmail.com>
2017-09-10 11:29:41 +0800

    可以读取文件

--- markdown/markdown_blog.go
+++ markdown/markdown_blog.go
@@ -12,6 +12,8 @@ import (
 	"time"
 )
 
+var path string
+
 type File struct {
 	name string
 	time time.Time
@@ -22,17 +24,51 @@ type Files []File
 func (a Files) Less(i, j int) bool { return a[j].time.Before(a[i].time) }
 func (a Files) Len() int           { return len(a) }
 func (a Files) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func getContent(name string) string {
+	file, err := ioutil.ReadFile(path + name + ".md")
+	if err != nil {
+		log.Fatal(err)
+		return err.Error()
+	}
+	s := string(file)
 
-// func preAndOld() {
-// 	file_infos := search(path, "bigzhu")
-// }
+	fmt.Printf(s)
+	return s
+	// content = name_file.read()
+	// name_file.close()
+	// if 'status: draft' in content:
+	//     return '# 这是一个机密文件, 不允许查看!'
+	// return content
+
+}
+
+func preAndOld(name string) (string, string) { //返回前一个文章后一个文章的名字
+	file_infos := search("")
+	file_name := name + ".md"
+	s_len := len(file_infos)
+	for index, value := range file_infos {
+		if value.name == file_name {
+			if s_len == 1 {
+				return "", ""
+			}
+			if index == 1 { // 第一个
+				return "", removeSuffix(file_infos[1].name)
+			}
+			if index == s_len-1 { // 最后一个
+				return removeSuffix(file_infos[index-1].name), ""
+			}
+			return removeSuffix(file_infos[index-1].name), removeSuffix(file_infos[index+1].name)
+		}
+	}
+
+	return "", ""
+}
 func removeSuffix(name string) string { // 删后缀
 	var extension = filepath.Ext(name)
 	name = name[0 : len(name)-len(extension)]
-	log.Printf(name)
 	return name
 }
-func search(path string, search_name string) []File {
+func search(search_name string) []File {
 	// 获取所有文件
 	var file_infos []File
 	files, _ := ioutil.ReadDir(path)
@@ -66,11 +102,15 @@ func main() {
 		return
 	}
 	var home = usr.HomeDir
-	var path = home + "/Dropbox/blog/"
-	file_infos := search(path, "bigzhu")
-	for index, value := range file_infos {
-		fmt.Printf("%s", index)
-		fmt.Printf(removeSuffix(value.name))
-		fmt.Printf(value.time.String())
-	}
+	path = home + "/Dropbox/blog/"
+	getContent("健身计划")
+	// pre, old := preAndOld("健身计划")
+	// fmt.Printf("%s\n", pre)
+	// fmt.Printf("%s\n", old)
+	// file_infos := search("bigzhu")
+	// for index, value := range file_infos {
+	// 	fmt.Printf("%s", index)
+	// 	fmt.Printf(removeSuffix(value.name))
+	// 	fmt.Printf(value.time.String())
+	// }
 }
